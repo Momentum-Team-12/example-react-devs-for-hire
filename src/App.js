a0db68b4e168a82a6b99dbd09bd492b66df9de03
Amy Gori <amydgori@gmail.com>
2022-05-20 14:57:15 -0400

    Use array of objects for data and move into separate file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import { useState } from 'react'
 import './App.css'
+import developerData from './developer-data'
 
 function App() {
-  const [devs, setDevs] = useState(['Jessica', 'Hillary', 'Stephen', 'Jamie'])
+  const [devs, setDevs] = useState(developerData)
 
   return (
     <>
       <h1>👩‍💻 Developers for Hire 👩‍💻</h1>
       {devs.map((dev, index) => (
-        <Developer name={dev} key={index} />
+        <Developer
+          name={dev.name}
+          expertise={dev.expertise}
+          available_for_hire={dev.available_for_hire}
+          key={index}
+        />
       ))}
     </>
   )
 }
 
-function Developer({ name }) {
+function Developer({ name, expertise, available_for_hire }) {
   return (
     <div className="dev">
       <p className="dev--name">{name}</p>
-      <p>Skills: Front-end and React</p>
+      <p>{expertise}</p>
     </div>
   )
 }
---
+++ src/developer-data.js
@@ -0,0 +1,24 @@
+const developerData = [
+  {
+    name: 'Jessica',
+    expertise: 'Front end, React',
+    available_for_hire: false,
+  },
+  {
+    name: 'Hillary',
+    expertise: 'Front end, React',
+    available_for_hire: true,
+  },
+  {
+    name: 'Stephen',
+    expertise: 'Front end, React',
+    available_for_hire: true,
+  },
+  {
+    name: 'Jamie',
+    expertise: 'Front end, React',
+    available_for_hire: true,
+  },
+]
+
+export default developerData
